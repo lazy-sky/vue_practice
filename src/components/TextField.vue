2f193db97f13503e5967df6d64de6c8a01be359a
lazy-sky <1cielo@naver.com>
2021-10-28 21:22:56 +0900

    Make TextField

---
+++ src/components/TextField.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="text" v-model="message" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
