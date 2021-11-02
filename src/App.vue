2b66c84cd5290309d43c2645366f899c85e24b01
lazy-sky <1cielo@naver.com>
2021-11-02 19:14:48 +0900

    Non prop

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
   <h1>{{ message }}</h1>
-  <TextField
-    v-model="message"
-    :count="count"
-  />
-  <TheButton
-    name="primary"
-    @click="count +=1"
-  >
-    Increase
-  </TheButton>
-  <TheButton
-    name="danger"
-    size="lg"
-    @click="count -=1"
-  >
-    <template #default>
-      New Button
-    </template>
-    <template #label="{ size, btnName }">
-      New Label / {{ size }} / {{ btnName }} 
-    </template>
-  </TheButton>
-  
-  <TheButton />
-  <button>Normal Button</button>
+  <HelloWorld class="active" />
 </template>
 
 <script>
-import TextField from '~/components/TextField'
-import TheButton from '~/components/TheButton'
+import HelloWorld from '~/components/HelloWorld'
+
 
 export default {
   components: {
-    TextField,
-    TheButton
+    HelloWorld
   },
 
   data () {
     return {
-      message: 'Hello Vue SFC!',
-      count: 7
+      message: 'Hello Vue'
     }
   }
 }
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="{ active: isActive }">
+    Hellow World
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  &.active {
+    color: red;
+  }
+}
+</style>
