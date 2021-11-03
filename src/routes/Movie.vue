63f7312fc5cfdc8fde9955602c02a8ce8716eccf
lazy-sky <1cielo@naver.com>
2021-11-03 21:32:38 +0900

    Dynamic router with async

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -2,3 +2,20 @@
   <h1>Movie page!</h1>
   <h2>{{ $route.params.sky }}</h2>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created() {
+    this.fetchMovie()  
+  },
+
+  methods: {
+    async fetchMovie() {
+      const { data } = await axios.get(`https://www.omdbapi.com/?apikey=7035c60c&i=${this.$route.params.sky}`)
+      console.log(data)
+    }
+  }
+}
+</script>
