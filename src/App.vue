3e71998b3ededaddce9f73223327a128c2a32177
lazy-sky <1cielo@naver.com>
2021-11-03 21:56:55 +0900

    click event to movies

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <template>
   <TheHeader />
+  <input
+    v-model="searchText"
+    type="text"
+    @keyup.enter="fetchMovies"
+  />
+  <div
+    v-for="movie in movies"
+    :key="movie.imdbID"
+    @click="moveTo(movie.imdbID)"
+  >
+    {{ movie.Title }}
+  </div>
   <RouterView />
 </template>
 
 <script>
+import axios from 'axios'
 import TheHeader from '~/components/TheHeader'
 
 export default {
   components: {
     TheHeader
+  },
+
+  data() {
+    return {
+      searchText: '',
+      movies: []
+    }
+  },
+
+  methods: {
+    async fetchMovies() {
+      const { data } = await axios.get(`https://www.omdbapi.com/?apikey=7035c60c&s=${this.searchText}`)
+      this.movies = data.Search
+    },
+
+    moveTo(id) {
+      this.$router.push(`/movies/${id}`)
+    }
   }
 }
 </script>
