26f33a56621082982d159c34af4a1d698f6d7537
lazy-sky <1cielo@naver.com>
2021-11-05 19:27:15 +0900

    Can go back to main page in not found

--- src/routes/NotFound.vue
+++ src/routes/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
   <h1>Page Not Found!</h1>
+  <RouterLink to="/">
+    메인페이지로 이동! (라우터)
+  </RouterLink>
+  <button @click="$router.push('/')">
+    메인페이지로 이동! (버튼)
+  </button>
 </template>
