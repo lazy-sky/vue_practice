756dcb25529a1dc44ce7e3b37ebec07926978aed
lazy-sky <1cielo@naver.com>
2021-11-05 21:06:36 +0900

    전역 등록

---
+++ src/components/TheModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <slot name="activator">
+  </slot>
+  <template v-if="modelValue">
+    <slot>
+    </slot>
+  </template>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import store from '~/store'
 import router from '~/routes'
 import '~/routes/guards'
+import TheModal from '~/components/TheModal'
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
+
+// 컴포넌트 전역 등록
+app.component('TheModal', TheModal)
 
 app.mount('#app')
